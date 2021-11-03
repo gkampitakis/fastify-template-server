d5b81e71d5cbf8a44881484acdf889ca675ccb19
Georgios Kampitakis <gkabitakis@gmail.com>
2021-11-03 18:43:26 +0000

    fix: failing tests

--- src/utils/config.spec.ts
+++ src/utils/config.spec.ts
@@ -10,21 +10,7 @@ describe('Configuration', () => {
       process.env.SERVICE = '';
       const { default: config } = await import('./config');
 
-      expect(config).toEqual({
-        server: {
-          port: 4000
-        },
-        isProduction: true,
-        cors: {
-          origin: false
-        },
-        healthCheck: {
-          exposeFailure: false,
-          info: { Service: '' },
-          path: '/api/health'
-        },
-        shutdownDelay: 5000
-      });
+      expect(config).toMatchSnapshot();
     });
 
     it('Logger', async () => {
@@ -32,14 +18,7 @@ describe('Configuration', () => {
 
       const { logger } = await import('./config');
 
-      expect(logger).toEqual({
-        level: 'warn',
-        base: {
-          name: 'test service'
-        },
-        enabled: true,
-        prettyPrint: false
-      });
+      expect(logger).toMatchSnapshot();
     });
   });
 
@@ -48,21 +27,7 @@ describe('Configuration', () => {
       process.env.NODE_ENV = 'dev';
       const { default: config } = await import('./config');
 
-      expect(config).toEqual({
-        server: {
-          port: 4000
-        },
-        isProduction: false,
-        cors: {
-          origin: false
-        },
-        healthCheck: {
-          exposeFailure: true,
-          info: { Service: 'test service' },
-          path: '/api/health'
-        },
-        shutdownDelay: 5000
-      });
+      expect(config).toMatchSnapshot();
     });
 
     it('Logger', async () => {
@@ -70,19 +35,7 @@ describe('Configuration', () => {
 
       const { logger } = await import('./config');
 
-      expect(logger).toEqual({
-        level: 'debug',
-        base: {
-          name: 'test service'
-        },
-        enabled: true,
-        prettyPrint: {
-          colorize: true,
-          ignore: 'hostname,pid',
-          translateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss",
-          levelFirst: true
-        }
-      });
+      expect(logger).toMatchSnapshot();
     });
   });
 });
